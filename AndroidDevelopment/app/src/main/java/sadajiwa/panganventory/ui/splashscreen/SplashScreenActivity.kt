6b13a846cbfc6af0afd8e2f88483087ef476b778
Dea Ivanka Malaha <dimalaha@users.noreply.github.com>
2021-05-22 13:52:33 +0800

    add Splash Screen (with animation)
    Signed-off-by: Dea Ivanka Malaha <dimalaha@users.noreply.github.com>

---
+++ AndroidDevelopment/app/src/main/java/sadajiwa/panganventory/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,47 @@
+package sadajiwa.panganventory.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import sadajiwa.panganventory.MainActivity
+import sadajiwa.panganventory.R
+import sadajiwa.panganventory.databinding.ActivitySplashScreenBinding
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            pvLogo.startAnimation(
+                AnimationUtils.loadAnimation(
+                    this@SplashScreenActivity,
+                    R.anim.splash_in
+                )
+            )
+            Handler(Looper.getMainLooper()).postDelayed({
+                pvLogo.startAnimation(
+                    AnimationUtils.loadAnimation(
+                        this@SplashScreenActivity,
+                        R.anim.splash_out
+                    )
+                )
+                Handler(Looper.getMainLooper()).postDelayed({
+                    pvLogo.visibility = View.GONE
+                    startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                    finish()
+                }, 500)
+            }, 1500)
+        }
+
+    }
+}
