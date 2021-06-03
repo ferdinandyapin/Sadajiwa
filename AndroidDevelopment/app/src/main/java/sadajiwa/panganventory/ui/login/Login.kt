54f8f011ec98a508324d40f893dee4711dd9b04e
Dea Ivanka Malaha <rdv.malaha@gmaill.com>
2021-06-03 22:42:02 +0800

    add login and register using Firebase Auth
    Signed-off-by: Dea Ivanka Malaha <rdv.malaha@gmaill.com>

--- AndroidDevelopment/app/src/main/java/sadajiwa/panganventory/ui/login/Login.kt
+++ AndroidDevelopment/app/src/main/java/sadajiwa/panganventory/ui/login/Login.kt
@@ -3,18 +3,49 @@ package sadajiwa.panganventory.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import sadajiwa.panganventory.R
+import sadajiwa.panganventory.databinding.ActivityLoginBinding
+import sadajiwa.panganventory.ui.main.MainActivity
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        val Register:TextView = findViewById(R.id.registertext)
-        Register.setOnClickListener{
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "Login"
+
+        binding.registertext.setOnClickListener{
             val intent = Intent(this,Register::class.java)
             startActivity(intent)
         }
 
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    fun login(view: View) {
+        with(binding) {
+            val email = binding.loginemail.text.toString()
+            val password = binding.loginpass.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    Intent(this@Login, MainActivity::class.java).also {
+                        startActivity(it)
+                    }
+                    finish()
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
--- AndroidDevelopment/app/src/main/java/sadajiwa/panganventory/ui/login/Register.kt
+++ AndroidDevelopment/app/src/main/java/sadajiwa/panganventory/ui/login/Register.kt
@@ -1,19 +1,47 @@
 package sadajiwa.panganventory.ui.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import sadajiwa.panganventory.R
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import sadajiwa.panganventory.databinding.ActivityRegisterBinding
+import sadajiwa.panganventory.ui.main.MainActivity
 
 class Register : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-        val Login: TextView = findViewById(R.id.logintext)
-        Login.setOnClickListener{
-            val intent = Intent(this,Login::class.java)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logintext.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
+        }
+
+        auth = FirebaseAuth.getInstance()
     }
-}
+
+    fun register(view : View) {
+        with(binding) {
+            val email = regemail.text.toString()
+            val password = regpass.textAlignment.toString()
+
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    Intent(this@Register, MainActivity::class.java).also {
+                        startActivity(it)
+                    }
+                    finish()
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 }
